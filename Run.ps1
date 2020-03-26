c1a358017795a359d7144e7bf1fdb5644b3dd962
Jacob <jwrenn4@outlook.com>
2020-03-26 10:49:54 -0400

    cleaning file

--- Run.ps1
+++ Run.ps1
@@ -7,4 +7,3 @@ for ($num=0; $num -lt 11; $num++){
     write-output 'Training GAN to create number $num'
     python $script $num $numepochs --epoch-size $epochsize -i $imagedir -m $modeldir
     }
-
